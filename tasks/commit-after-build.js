b810f8052225be63bf2961629b6c9da229b5d4de
Anders Riutta <git@andersriutta.com>
2015-05-29 17:14:15 -0700

    Use correct file paths.

--- tasks/commit-after-build.js
+++ tasks/commit-after-build.js
@@ -4,10 +4,13 @@ var git = require('gulp-git');
 var gitStreaming = require('../util/git-streaming.js');
 var gulp = require(__dirname + '/../../gulp');
 var highland = require('highland');
+
+var parentPackageDir = __dirname + '/../../../';
+
 var metadataFilePaths = config.metadataFilePaths;
 
 gulp.task('commit-after-build', function commitAfterBuild(callback) {
-  var package = JSON.parse(fs.readFileSync('package.json'));
+  var package = JSON.parse(fs.readFileSync(parentPackageDir + 'package.json'));
   var version = package.version;
 
   gulp.src(['./dist/',
@@ -16,16 +19,22 @@ gulp.task('commit-after-build', function commitAfterBuild(callback) {
             './test/jquery-demos/',
             './test/web-component-demos/',
             './demo/',
-            'README.md']
+            './README.md']
             .concat(metadataFilePaths)
+            .map(function(path) {
+              return parentPackageDir + path;
+            })
   )
   .pipe(highland.pipeline())
-  .through(git.add())
-  .through(git.commit('Built and bumped version to ' + version + '.'))
-  .last()
   .errors(function(err, push) {
     throw err;
   })
+  .through(git.add())
+  .through(git.commit('Built and bumped version to ' + version + '.'))
+  .errors(function(err, push) {
+    throw err;
+  })
+  .last()
   .each(function() {
     return callback();
   });
